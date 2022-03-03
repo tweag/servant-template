5ebde7624a84f531d0447688f717ec29c9db29bd
Marco Perone <marco.perone@tweag.io>
2022-03-03 17:43:18 +0100

    improve handling of errors

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,14 @@ module Main where
 
 import Api.Application (app)
 import Api.AppServices (AppServices(..))
-import qualified Infrastructure.Authentication.AuthenticateUser as Auth (AuthenticateUser(AuthenticateUser), authenticateUser, hoistAuthenticateUser)
+import qualified Infrastructure.Authentication.AuthenticateUser as Auth (AuthenticateUser(AuthenticateUser), AuthenticationError(..), authenticateUser, hoistAuthenticateUser)
 import Infrastructure.Persistence.PostgresContentRepository (postgresContentRepository)
 import Infrastructure.Persistence.PostgresUserRepository (postgresUserRepository)
 import Tagger.ContentRepository (ContentRepository, hoistContentRepository)
 import Tagger.UserRepository (UserRepository, hoistUserRepository)
 
 -- base
+import Control.Monad ((<=<))
 import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (fromMaybe)
 
@@ -23,8 +24,11 @@ import Hasql.Connection (Connection, acquire)
 -- jose
 import Crypto.JOSE.JWK (JWK)
 
+-- mtl
+import Control.Monad.Except (throwError)
+
 -- servant
-import Servant (Handler)
+import Servant (Handler, err401, err500)
 
 -- servant-auth-server
 import Servant.Auth.Server (defaultJWTSettings, generateKey)
@@ -35,19 +39,22 @@ import Control.Monad.Trans.Except (ExceptT, runExceptT)
 -- warp
 import Network.Wai.Handler.Warp (run)
 
--- TODO: is `fail` the correct thing to use here?
-eitherTtoHandler :: Show e => ExceptT e IO a -> Handler a
-eitherTtoHandler = liftIO . (=<<) (either (fail . show) pure) . runExceptT
+eitherTToHandler :: (e -> Handler a) -> ExceptT e IO a -> Handler a
+eitherTToHandler handleError = either handleError pure <=< liftIO . runExceptT
 
 -- TODO: the whole hoist stuff could probably be managed in a better way
 connectedContentRepository :: Connection -> ContentRepository Handler
-connectedContentRepository = hoistContentRepository eitherTtoHandler . postgresContentRepository
+connectedContentRepository = hoistContentRepository (eitherTToHandler . const $ throwError err500) . postgresContentRepository
 
 connectedUserRepository :: Connection -> UserRepository Handler
-connectedUserRepository = hoistUserRepository eitherTtoHandler . postgresUserRepository
+connectedUserRepository = hoistUserRepository (eitherTToHandler . const $ throwError err500). postgresUserRepository
 
 connectedAuthenticateUser :: Connection -> Auth.AuthenticateUser Handler
-connectedAuthenticateUser = Auth.hoistAuthenticateUser eitherTtoHandler . Auth.AuthenticateUser . Auth.authenticateUser
+connectedAuthenticateUser = Auth.hoistAuthenticateUser (eitherTToHandler handleAuthenticationError) . Auth.AuthenticateUser . Auth.authenticateUser
+  where
+    handleAuthenticationError :: Auth.AuthenticationError -> Handler a
+    handleAuthenticationError (Auth.AuthenticationQueryError _) = throwError err500
+    handleAuthenticationError _                                 = throwError err401
 
 appServices :: Connection -> JWK -> AppServices
 appServices connection key = AppServices
