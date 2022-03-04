87e6abe03e5bf1850473ee4b6c06f0af2acd2958
Marco Perone <marco.perone@tweag.io>
2022-03-04 09:28:14 +0100

    move intialization of services to AppServices

--- app/Main.hs
+++ app/Main.hs
@@ -3,75 +3,22 @@
 module Main where
 
 import Api.Application (app)
-import Api.AppServices (AppServices(..))
-import qualified Infrastructure.Authentication.AuthenticateUser as Auth (AuthenticateUser(AuthenticateUser), AuthenticationError(..), authenticateUser, hoistAuthenticateUser)
-import Infrastructure.Authentication.PasswordManager (PasswordManager, PasswordManagerError(..), bcryptPasswordManager, hoistPasswordManager)
-import Infrastructure.Persistence.PostgresContentRepository (postgresContentRepository)
-import Infrastructure.Persistence.PostgresUserRepository (postgresUserRepository)
-import Tagger.ContentRepository (ContentRepository, hoistContentRepository)
-import Tagger.UserRepository (UserRepository, hoistUserRepository)
+import Api.AppServices (appServices)
 
 -- base
-import Control.Monad ((<=<))
-import Control.Monad.IO.Class (liftIO)
 import Data.Maybe (fromMaybe)
 
 -- bytestring
 import Data.ByteString.Char8 (unpack)
 
 -- hasql
-import Hasql.Connection (Connection, acquire)
-
--- jose
-import Crypto.JOSE.JWK (JWK)
-
--- mtl
-import Control.Monad.Except (throwError)
-
--- servant
-import Servant (Handler, err401, err500)
+import Hasql.Connection (acquire)
 
 -- servant-auth-server
-import Servant.Auth.Server (JWTSettings, defaultJWTSettings, generateKey)
-
--- transformers
-import Control.Monad.Trans.Except (ExceptT, runExceptT)
+import Servant.Auth.Server (generateKey)
 
 -- warp
 import Network.Wai.Handler.Warp (run)
-
-eitherTToHandler :: (e -> Handler a) -> ExceptT e IO a -> Handler a
-eitherTToHandler handleError = either handleError pure <=< liftIO . runExceptT
-
--- TODO: the whole hoist stuff could probably be managed in a better way
-connectedContentRepository :: Connection -> ContentRepository Handler
-connectedContentRepository = hoistContentRepository (eitherTToHandler . const $ throwError err500) . postgresContentRepository
-
-connectedUserRepository :: Connection -> UserRepository Handler
-connectedUserRepository = hoistUserRepository (eitherTToHandler . const $ throwError err500). postgresUserRepository
-
-connectedAuthenticateUser :: Connection -> Auth.AuthenticateUser Handler
-connectedAuthenticateUser = Auth.hoistAuthenticateUser (eitherTToHandler handleAuthenticationError) . Auth.AuthenticateUser . Auth.authenticateUser
-  where
-    handleAuthenticationError :: Auth.AuthenticationError -> Handler a
-    handleAuthenticationError (Auth.AuthenticationQueryError _) = throwError err500
-    handleAuthenticationError _                                 = throwError err401
-
-encryptedPasswordManager :: JWTSettings -> PasswordManager Handler
-encryptedPasswordManager = hoistPasswordManager (eitherTToHandler handlePasswordManagerError) . bcryptPasswordManager
-  where
-    handlePasswordManagerError :: PasswordManagerError -> Handler a
-    handlePasswordManagerError FailedHashing         = throwError err500
-    handlePasswordManagerError (FailedJWTCreation _) = throwError err401
-
-appServices :: Connection -> JWK -> AppServices
-appServices connection key = AppServices
-  { jwtSettings       = defaultJWTSettings key
-  , passwordManager   = encryptedPasswordManager $ defaultJWTSettings key
-  , contentRepository = connectedContentRepository connection
-  , userRepository    = connectedUserRepository connection
-  , authenticateUser  = connectedAuthenticateUser connection
-  }
 
 main:: IO ()
 main = do
--- src/Api/AppServices.hs
+++ src/Api/AppServices.hs
@@ -1,20 +1,70 @@
 module Api.AppServices where
 
-import Infrastructure.Authentication.AuthenticateUser (AuthenticateUser)
-import Infrastructure.Authentication.PasswordManager (PasswordManager)
-import Tagger.ContentRepository (ContentRepository)
-import Tagger.UserRepository (UserRepository)
+import qualified Infrastructure.Authentication.AuthenticateUser as Auth (AuthenticateUser(AuthenticateUser), AuthenticationError(AuthenticationQueryError), authenticateUser, hoistAuthenticateUser)
+import Infrastructure.Authentication.PasswordManager (PasswordManager, PasswordManagerError(..), hoistPasswordManager, bcryptPasswordManager)
+import Infrastructure.Persistence.PostgresContentRepository (postgresContentRepository)
+import Infrastructure.Persistence.PostgresUserRepository (postgresUserRepository)
+import Tagger.ContentRepository (ContentRepository, hoistContentRepository)
+import Tagger.UserRepository (UserRepository, hoistUserRepository)
+
+-- base
+import Control.Monad ((<=<))
+import Control.Monad.IO.Class (liftIO)
+
+-- hasql
+import Hasql.Connection (Connection)
+
+-- jose
+import Crypto.JOSE.JWK (JWK)
+
+-- mtl
+import Control.Monad.Except (throwError)
 
 -- servant-auth-server
-import Servant.Auth.Server (JWTSettings)
+import Servant.Auth.Server (JWTSettings, defaultJWTSettings)
 
 -- servant-server
-import Servant (Handler)
+import Servant (Handler, err500, err401)
+
+-- transformers
+import Control.Monad.Trans.Except (ExceptT, runExceptT)
 
 data AppServices = AppServices
   { jwtSettings       :: JWTSettings
   , passwordManager   :: PasswordManager Handler
   , contentRepository :: ContentRepository Handler
   , userRepository    :: UserRepository Handler
-  , authenticateUser  :: AuthenticateUser Handler
+  , authenticateUser  :: Auth.AuthenticateUser Handler
+  }
+
+eitherTToHandler :: (e -> Handler a) -> ExceptT e IO a -> Handler a
+eitherTToHandler handleError = either handleError pure <=< liftIO . runExceptT
+
+connectedContentRepository :: Connection -> ContentRepository Handler
+connectedContentRepository = hoistContentRepository (eitherTToHandler . const $ throwError err500) . postgresContentRepository
+
+connectedUserRepository :: Connection -> UserRepository Handler
+connectedUserRepository = hoistUserRepository (eitherTToHandler . const $ throwError err500). postgresUserRepository
+
+connectedAuthenticateUser :: Connection -> Auth.AuthenticateUser Handler
+connectedAuthenticateUser = Auth.hoistAuthenticateUser (eitherTToHandler handleAuthenticationError) . Auth.AuthenticateUser . Auth.authenticateUser
+  where
+    handleAuthenticationError :: Auth.AuthenticationError -> Handler a
+    handleAuthenticationError (Auth.AuthenticationQueryError _) = throwError err500
+    handleAuthenticationError _                                 = throwError err401
+
+encryptedPasswordManager :: JWTSettings -> PasswordManager Handler
+encryptedPasswordManager = hoistPasswordManager (eitherTToHandler handlePasswordManagerError) . bcryptPasswordManager
+  where
+    handlePasswordManagerError :: PasswordManagerError -> Handler a
+    handlePasswordManagerError FailedHashing         = throwError err500
+    handlePasswordManagerError (FailedJWTCreation _) = throwError err401
+
+appServices :: Connection -> JWK -> AppServices
+appServices connection key = AppServices
+  { jwtSettings       = defaultJWTSettings key
+  , passwordManager   = encryptedPasswordManager $ defaultJWTSettings key
+  , contentRepository = connectedContentRepository connection
+  , userRepository    = connectedUserRepository connection
+  , authenticateUser  = connectedAuthenticateUser connection
   }
