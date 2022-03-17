82d5c125b034c95e3cb466d9a918793ed230b314
Marco Perone <marco.perone@tweag.io>
2022-03-17 17:28:53 +0100

    factor out code related to login and registration

---
+++ elm/src/Credentials.elm
@@ -0,0 +1,87 @@
+module Credentials exposing (..)
+
+-- elm/html
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+
+-- elm/http
+import Http exposing (..)
+
+-- elm/json
+import Json.Encode exposing (..)
+import Json.Decode exposing (..)
+
+-- MODEL
+
+type alias Credentials =
+  { username : String
+  , password : String
+  }
+
+emptyCredentials : Credentials
+emptyCredentials =
+  { username = ""
+  , password = ""
+  }
+
+type Submit a
+  = NotYetSubmitted
+  | Successful a
+  | Failure Http.Error
+
+-- UPDATE
+
+type CredentialsMessage
+  = Username String
+  | Password String
+
+updateCredentials : CredentialsMessage -> Credentials -> Credentials
+updateCredentials credentialsMessage credentials =
+  case credentialsMessage of
+    Username name     -> { credentials | username = name }
+    Password password -> { credentials | password = password }
+
+type SubmitMessage a
+  = Submit
+  | Failed Http.Error
+  | Succeeded a
+
+updateSubmit : Decoder a -> String -> Credentials -> SubmitMessage a -> Submit a -> ( Submit a, Cmd (SubmitMessage a) )
+updateSubmit decoder url credentials submitMessage model =
+  case submitMessage of
+    Submit          -> ( model, submit decoder url credentials )
+    Failed error    -> ( Failure error, Cmd.none )
+    Succeeded value -> ( Successful value, Cmd.none )
+
+-- VIEW
+
+credentialsForm : (CredentialsMessage -> msg) -> (SubmitMessage a -> msg) -> Credentials -> Html msg
+credentialsForm liftModel liftMessage credentials = div []
+  [ Html.map liftModel (div []
+    [ viewInput "text"     "Username" credentials.username Username
+    , viewInput "password" "Password" credentials.password Password
+    ])
+  , button [ onClick (liftMessage Submit) ] [ text "Submit" ]
+  ]
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+  input [ type_ t, placeholder p, Html.Attributes.value v, onInput toMsg ] []
+
+-- HTTP
+
+submit : Decoder a -> String -> Credentials -> Cmd ( SubmitMessage a )
+submit decoder url register = Http.post
+  { url = url
+  , body = jsonBody ( Json.Encode.object
+    [ ( "username", Json.Encode.string register.username )
+    , ( "password", Json.Encode.string register.password )
+    ])
+  , expect = expectJson handleSubmitResponse decoder
+  }
+
+handleSubmitResponse : Result Http.Error a -> SubmitMessage a
+handleSubmitResponse result = case result of
+  Ok value  -> Succeeded value
+  Err error -> Failed error
--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -1,11 +1,14 @@
 module Main exposing (..)
 
+import Credentials exposing (..)
+
+-- elm/browser
 import Browser
+
+-- elm/html
 import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Http exposing (..)
-import Json.Encode exposing (..)
+
+-- elm/json
 import Json.Decode exposing (..)
 
 -- MAIN
@@ -25,22 +28,6 @@ type alias UserId = String
 
 type alias Token = String
 
-type alias Credentials =
-  { username : String
-  , password : String
-  }
-
-emptyCredentials : Credentials
-emptyCredentials =
-  { username = ""
-  , password = ""
-  }
-
-type Submit a
-  = NotYetSubmitted
-  | Successful a
-  | Failure Http.Error
-
 type alias Model =
   { register : Credentials
   , login    : Credentials
@@ -49,37 +36,22 @@ type alias Model =
   }
 
 init : () -> ( Model, Cmd Msg )
-init _ = ( Model emptyCredentials emptyCredentials NotYetSubmitted NotYetSubmitted, Cmd.none )
+init _ =
+  ( { register = emptyCredentials
+    , login = emptyCredentials
+    , registerSubmit = NotYetSubmitted
+    , loginSubmit = NotYetSubmitted
+    }
+  , Cmd.none
+  )
 
 -- UPDATE
-
-type CredentialsMessage
-  = Username String
-  | Password String
-
-type SubmitMessage a
-  = Submit
-  | Failed Http.Error
-  | Succeeded a
 
 type Msg
   = RegisterData CredentialsMessage
   | LoginData CredentialsMessage
   | Register (SubmitMessage UserId)
   | Login (SubmitMessage Token)
-
-updateCredentials : CredentialsMessage -> Credentials -> Credentials
-updateCredentials credentialsMessage credentials =
-  case credentialsMessage of
-    Username name     -> { credentials | username = name }
-    Password password -> { credentials | password = password }
-
-updateSubmit : Decoder a -> String -> Credentials -> SubmitMessage a -> Submit a -> ( Submit a, Cmd (SubmitMessage a) )
-updateSubmit decoder url credentials submitMessage model =
-  case submitMessage of
-    Submit          -> ( model, submit decoder url credentials )
-    Failed error    -> ( Failure error, Cmd.none )
-    Succeeded value -> ( Successful value, Cmd.none )
 
 updateModelWithRegisterSubmit : Model -> Submit UserId -> Model
 updateModelWithRegisterSubmit model registerSubmit = { model | registerSubmit = registerSubmit }
@@ -122,35 +94,7 @@ view model = div []
     ]
   ]
 
-credentialsForm : (CredentialsMessage -> Msg) -> (SubmitMessage a -> Msg) -> Credentials -> Html Msg
-credentialsForm liftModel liftMessage credentials = div []
-  [ Html.map liftModel (div []
-    [ viewInput "text"     "Username" credentials.username Username
-    , viewInput "password" "Password" credentials.password Password
-    ])
-  , button [ onClick (liftMessage Submit) ] [ text "Submit" ]
-  ]
-
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-  input [ type_ t, placeholder p, Html.Attributes.value v, onInput toMsg ] []
-
 -- HTTP
-
-submit : Decoder a -> String -> Credentials -> Cmd ( SubmitMessage a )
-submit decoder url register = Http.post
-  { url = url
-  , body = jsonBody ( Json.Encode.object
-    [ ( "username", Json.Encode.string register.username )
-    , ( "password", Json.Encode.string register.password )
-    ])
-  , expect = expectJson handleSubmitResponse decoder
-  }
-
-handleSubmitResponse : Result Http.Error a -> SubmitMessage a
-handleSubmitResponse result = case result of
-  Ok value  -> Succeeded value
-  Err error -> Failed error
 
 userIdDecoder : Decoder UserId
 userIdDecoder = Json.Decode.string
