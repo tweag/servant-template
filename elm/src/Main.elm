9b3808d840c8e9eec098df8c6ea61cd9b43134d2
Marco Perone <marco.perone@tweag.io>
2022-03-17 15:45:33 +0100

    elm client is able to register and login users

--- elm/src/Main.elm
+++ elm/src/Main.elm
@@ -23,33 +23,83 @@ main =
 
 type alias UserId = String
 
-type alias Model =
+type alias Token = String
+
+type alias Credentials =
   { username : String
   , password : String
-  , userId   : Maybe UserId
-  , error    : Maybe Http.Error
+  }
+
+emptyCredentials : Credentials
+emptyCredentials =
+  { username = ""
+  , password = ""
+  }
+
+type Submit a
+  = NotYetSubmitted
+  | Successful a
+  | Failure Http.Error
+
+type alias Model =
+  { register : Credentials
+  , login    : Credentials
+  , registerSubmit : Submit UserId
+  , loginSubmit : Submit Token
   }
 
 init : () -> ( Model, Cmd Msg )
-init _ = ( Model "" "" Nothing Nothing, Cmd.none )
+init _ = ( Model emptyCredentials emptyCredentials NotYetSubmitted NotYetSubmitted, Cmd.none )
 
 -- UPDATE
 
-type Msg
+type CredentialsMessage
   = Username String
   | Password String
-  | Submit
-  | SubmitFailed Http.Error
-  | SubmitSucceeded UserId
+
+type SubmitMessage a
+  = Submit
+  | Failed Http.Error
+  | Succeeded a
+
+type Msg
+  = RegisterData CredentialsMessage
+  | LoginData CredentialsMessage
+  | Register (SubmitMessage UserId)
+  | Login (SubmitMessage Token)
+
+updateCredentials : CredentialsMessage -> Credentials -> Credentials
+updateCredentials credentialsMessage credentials =
+  case credentialsMessage of
+    Username name     -> { credentials | username = name }
+    Password password -> { credentials | password = password }
+
+updateSubmit : Decoder a -> String -> Credentials -> SubmitMessage a -> Submit a -> ( Submit a, Cmd (SubmitMessage a) )
+updateSubmit decoder url credentials submitMessage model =
+  case submitMessage of
+    Submit          -> ( model, submit decoder url credentials )
+    Failed error    -> ( Failure error, Cmd.none )
+    Succeeded value -> ( Successful value, Cmd.none )
+
+updateModelWithRegisterSubmit : Model -> Submit UserId -> Model
+updateModelWithRegisterSubmit model registerSubmit = { model | registerSubmit = registerSubmit }
+
+updateModelWithLoginSubmit : Model -> Submit Token -> Model
+updateModelWithLoginSubmit model loginSubmit = { model | loginSubmit = loginSubmit}
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Username name          -> ( { model | username = name }, Cmd.none )
-    Password password      -> ( { model | password = password }, Cmd.none )
-    Submit                 -> ( model, submit model )
-    SubmitFailed error     -> ( { model | error = Just error }, Cmd.none )
-    SubmitSucceeded userId -> ( { model | userId = Just userId }, Cmd.none )
+    RegisterData credentialsMessage -> ( { model | register = updateCredentials credentialsMessage model.register }, Cmd.none )
+    LoginData credentialsMessage    -> ( { model | login = updateCredentials credentialsMessage model.login }, Cmd.none )
+    Register registerMessage        -> Tuple.mapBoth
+      (updateModelWithRegisterSubmit model)
+      (Cmd.map Register)
+      (updateSubmit userIdDecoder "http://localhost:8080/register" model.register registerMessage model.registerSubmit)
+    Login loginMessage              -> Tuple.mapBoth
+      ( updateModelWithLoginSubmit model )
+      (Cmd.map Login)
+      (updateSubmit tokenDecoder "http://localhost:8080/login" model.login loginMessage model.loginSubmit)
 
 -- SUBSCRIPTIONS
 
@@ -61,9 +111,24 @@ subscriptions _ =
 
 view : Model -> Html Msg
 view model = div []
-  [ viewInput "text"     "Username" model.username Username
-  , viewInput "password" "Password" model.password Password
-  , button [ onClick Submit ] [ text "Submit" ]
+  [ h1 [] [ text "Tagger" ]
+  , div []
+    [ h2 [] [ text "Register User" ]
+    , credentialsForm RegisterData Register model.register
+    ]
+  , div []
+    [ h2 [] [ text "Login" ]
+    , credentialsForm LoginData Login model.login
+    ]
+  ]
+
+credentialsForm : (CredentialsMessage -> Msg) -> (SubmitMessage a -> Msg) -> Credentials -> Html Msg
+credentialsForm liftModel liftMessage credentials = div []
+  [ Html.map liftModel (div []
+    [ viewInput "text"     "Username" credentials.username Username
+    , viewInput "password" "Password" credentials.password Password
+    ])
+  , button [ onClick (liftMessage Submit) ] [ text "Submit" ]
   ]
 
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
@@ -72,20 +137,23 @@ viewInput t p v toMsg =
 
 -- HTTP
 
-submit : Model -> Cmd Msg
-submit model = Http.post
-  { url = "http://localhost:8080/register"
+submit : Decoder a -> String -> Credentials -> Cmd ( SubmitMessage a )
+submit decoder url register = Http.post
+  { url = url
   , body = jsonBody ( Json.Encode.object
-    [ ( "username", Json.Encode.string model.username )
-    , ( "password", Json.Encode.string model.password )
+    [ ( "username", Json.Encode.string register.username )
+    , ( "password", Json.Encode.string register.password )
     ])
-  , expect = expectJson handleSubmitResponse userIdDecoder
+  , expect = expectJson handleSubmitResponse decoder
   }
 
-handleSubmitResponse : Result Http.Error UserId -> Msg
+handleSubmitResponse : Result Http.Error a -> SubmitMessage a
 handleSubmitResponse result = case result of
-  Ok userId -> SubmitSucceeded userId
-  Err error -> SubmitFailed error
+  Ok value  -> Succeeded value
+  Err error -> Failed error
 
 userIdDecoder : Decoder UserId
 userIdDecoder = Json.Decode.string
+
+tokenDecoder : Decoder Token
+tokenDecoder = Json.Decode.string
