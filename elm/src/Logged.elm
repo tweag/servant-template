b427f234b49f01997cd0ae2bcd95f1ea77aa66e6
Marco Perone <marco.perone@tweag.io>
2022-03-22 15:31:06 +0100

    complete calling get contents endpoint

--- elm/src/Logged.elm
+++ elm/src/Logged.elm
@@ -1,8 +1,16 @@
 module Logged exposing (..)
 
+import Anonymous exposing (Token)
+
 -- elm/http
 import Http exposing (..)
+
+-- elm/json
 import Json.Decode exposing (..)
+
+-- elm/url
+import Url exposing (..)
+import Url.Builder exposing (toQuery)
 
 -- MODEL
 
@@ -24,10 +32,25 @@ type Msg
 
 -- HTTP
 
-retrieveContents : Cmd Msg
-retrieveContents = Http.get
-  { url    = "http://localhost:8080/get-contents"
-  , expect = expectJson handleContentsResponse ( list contentDecoder )
+retrieveUrl : List Tag -> Url
+retrieveUrl tags =
+  { protocol = Http
+  , host     = "localhost"
+  , port_    = Just 8080
+  , path     = "/get-contents"
+  , query    = Just ( toQuery ( List.map ( \{name} -> Url.Builder.string "tag" name) tags ) )
+  , fragment = Nothing
+  }
+
+retrieveContents : Token -> List Tag -> Cmd Msg
+retrieveContents token tags = Http.request
+  { method  = "GET"
+  , headers = [ header "Authorization" ( String.append "Bearer " token ) ]
+  , url     = Url.toString ( retrieveUrl tags )
+  , body    = emptyBody
+  , expect  = expectJson handleContentsResponse ( list contentDecoder )
+  , timeout = Nothing
+  , tracker = Nothing
   }
 
 handleContentsResponse : Result Http.Error ( List Content ) -> Msg
