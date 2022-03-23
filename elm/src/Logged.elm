2a31c1e0ad4f3e0dbeee7e17bcee0067feb7f539
Marco Perone <marco.perone@tweag.io>
2022-03-23 17:57:07 +0100

    add new content

--- elm/src/Logged.elm
+++ elm/src/Logged.elm
@@ -12,6 +12,7 @@ import Http exposing (..)
 
 -- elm/json
 import Json.Decode exposing (..)
+import Json.Encode exposing (..)
 
 -- elm/url
 import Url exposing (..)
@@ -48,6 +49,8 @@ type Msg
   | NewTag String
   | SubmitTag
   | SubmitContent
+  | SubmitSuccessful Content
+  | SubmitFailed
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model = case msg of
@@ -56,7 +59,9 @@ update msg model = case msg of
   NewContent newContent    -> ( { model | newContent = newContent }, Cmd.none )
   NewTag newTag            -> ( { model | newTag = newTag }, Cmd.none )
   SubmitTag                -> ( { model | newTags = ( Tag model.newTag ) :: model.newTags, newTag = "" }, Cmd.none )
-  SubmitContent            -> ( model, Cmd.none )
+  SubmitContent            -> ( model, addContent model.token ( Content model.newContent model.newTags ) )
+  SubmitSuccessful content -> ( { model | contents = content :: model.contents }, Cmd.none )
+  SubmitFailed             -> ( model, Cmd.none )
 
 -- VIEW
 
@@ -107,13 +112,16 @@ retrieveUrl tags =
   , fragment = Nothing
   }
 
+authorization : Token -> Header
+authorization token = Http.header "Authorization" ( String.append "Bearer " token )
+
 retrieveContents : Token -> List Tag -> Cmd Msg
 retrieveContents token tags = Http.request
   { method  = "GET"
-  , headers = [ Http.header "Authorization" ( String.append "Bearer " token ) ]
+  , headers = [ authorization token ]
   , url     = Url.toString ( retrieveUrl tags )
   , body    = emptyBody
-  , expect  = expectJson handleContentsResponse ( list wrappedContentDecoder )
+  , expect  = expectJson handleContentsResponse ( Json.Decode.list wrappedContentDecoder )
   , timeout = Nothing
   , tracker = Nothing
   }
@@ -125,13 +133,38 @@ handleContentsResponse result = case result of
 
 tagDecoder : Decoder Tag
 tagDecoder = Json.Decode.map Tag
-  ( field "name" string )
+  ( field "name" Json.Decode.string )
 
 contentDecoder : Decoder Content
 contentDecoder = map2 Content
-  ( field "message" string )
-  ( field "tags"    ( list ( Json.Decode.map Tag string )))
+  ( field "message" Json.Decode.string )
+  ( field "tags"    ( Json.Decode.list ( Json.Decode.map Tag Json.Decode.string )))
 
 wrappedContentDecoder : Decoder Content
 wrappedContentDecoder = Json.Decode.map identity
   ( field "content" contentDecoder )
+
+addContent : Token -> Content -> Cmd Msg
+addContent token content = Http.request
+  { method = "POST"
+  , headers = [ authorization token ]
+  , url = "http://localhost:8080/add-content"
+  , body = jsonBody ( contentEncoder content )
+  , expect = expectWhatever ( handleNewContentResponse content )
+  , timeout = Nothing
+  , tracker = Nothing
+  }
+
+handleNewContentResponse : Content -> Result Http.Error () -> Msg
+handleNewContentResponse content result = case result of
+  Err _ -> SubmitFailed
+  Ok () -> SubmitSuccessful content
+
+tagEncoder : Tag -> Json.Encode.Value
+tagEncoder tag = Json.Encode.string tag.name
+
+contentEncoder : Content -> Json.Encode.Value
+contentEncoder content = Json.Encode.object
+  [ ( "message", Json.Encode.string content.message )
+  , ( "tags", Json.Encode.list tagEncoder content.tags)
+  ]
