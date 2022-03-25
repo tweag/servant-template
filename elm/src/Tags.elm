8610b66c21869d10d28aeeaa61f310617be11810
Marco Perone <marco.perone@tweag.io>
2022-03-25 18:00:15 +0100

    isolate the update for Tags

--- elm/src/Tags.elm
+++ elm/src/Tags.elm
@@ -4,16 +4,25 @@ import LoggedModel exposing (..)
 
 -- MODEL
 
-type alias Tags =
+type alias Model =
   { newTag : String
   , tags : List Tag
   }
 
-init : Tags
-init = Tags "" []
+init : Model
+init = Model "" []
 
 -- UPDATE
 
 type Msg
   = NewTag String
   | Submit
+
+update : ( List Tag -> Cmd msg ) -> Msg -> Model -> ( Model, Cmd msg )
+update onSubmit msg model = case msg of
+  NewTag newTag -> ( { model | newTag = newTag }, Cmd.none )
+  Submit        ->
+    let
+      newTags = ( Tag model.newTag ) :: model.tags
+    in
+      ( { model | newTag = "", tags = newTags }, onSubmit newTags )
