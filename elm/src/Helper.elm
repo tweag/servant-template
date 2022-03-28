1160349be1e99c81c0999a846538c209ba2ea387
Marco Perone <marco.perone@tweag.io>
2022-03-28 10:35:12 +0200

    allow to remove added tags

--- elm/src/Helper.elm
+++ elm/src/Helper.elm
@@ -8,3 +8,11 @@ import Html.Events exposing (..)
 viewInput : String -> String -> String -> (String -> msg) -> Html msg
 viewInput t p v toMsg =
   input [ type_ t, placeholder p, Html.Attributes.value v, onInput toMsg ] []
+
+remove : a -> List a -> List a
+remove value list = case list of
+  []               -> []
+  ( head :: tail ) ->
+    if   head == value
+    then remove value tail
+    else head :: remove value tail
--- elm/src/Tags.elm
+++ elm/src/Tags.elm
@@ -22,23 +22,41 @@ init = Model "" []
 type Msg
   = NewTag String
   | Submit
+  | Remove String
 
 update : ( List Tag -> Cmd msg ) -> Msg -> Model -> ( Model, Cmd msg )
 update onSubmit msg model = case msg of
   NewTag newTag -> ( { model | newTag = newTag }, Cmd.none )
   Submit        ->
     let
-      newTags = ( Tag model.newTag ) :: model.tags
+      tags = ( Tag model.newTag ) :: model.tags
     in
-      ( { model | newTag = "", tags = newTags }, onSubmit newTags )
+      ( { model | newTag = "", tags = tags }, onSubmit tags )
+  Remove id     ->
+    let
+      tags = removeTag id model.tags
+    in
+      ( { model | tags = tags }, onSubmit tags )
+
+removeTag : String -> List Tag -> List Tag
+removeTag id tags = remove ( Tag id ) tags
 
 -- VIEW
+
+removable : String -> Html Msg -> Html Msg
+removable id html = div []
+  [ html
+  , div [ onClick ( Remove id ) ] [ text "x" ]
+  ]
+
+viewRemovableTag : ( Tag -> Html Msg ) -> Tag -> Html Msg
+viewRemovableTag viewTag tag = removable tag.name ( viewTag tag )
 
 view : ( Tag -> Html Msg ) -> String -> String -> String -> Model -> Html Msg
 view viewTag header filterText submitText model = div []
   [ h3 [] [ text header ]
   , div []
-    ( List.map viewTag model.tags )
+    ( List.map ( viewRemovableTag viewTag ) model.tags )
   , viewInput "text" filterText model.newTag NewTag
   , button [ onClick Submit ] [ text submitText ]
   ]
