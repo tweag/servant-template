08db2b48fdfd721126af246ac561e89502d00c37
Christian Georgii <christian.georgii@tweag.io>
2022-05-25 13:32:49 +0200

    Make stack+nix integration unobtrusive

--- shell.nix
+++ shell.nix
@@ -1,6 +1,22 @@
 with (import <nixpkgs> {});
+let
+  stack-wrapped = pkgs.symlinkJoin {
+    name = "stack";
+    paths = [ pkgs.stack ];
+    buildInputs = [ pkgs.makeWrapper ];
+    postBuild = ''
+      wrapProgram $out/bin/stack \
+        --add-flags "\
+          --nix \
+          --no-nix-pure \
+          --nix-shell-file=nix/stack-integration.nix \
+        "
+    '';
+  };
+in
 mkShell {
   name = "tagger-dev";
+  NIX_PATH = "nixpkgs=" + pkgs.path;
   buildInputs = with pkgs; [
     elmPackages.elm
     elmPackages.elm-language-server
@@ -8,7 +24,7 @@ mkShell {
     haskellPackages.ormolu
     nodePackages.npm
     postgresql
-    stack
+    stack-wrapped
     watchexec
     zlib
   ];
