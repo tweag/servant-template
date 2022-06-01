f8f79699fea3d7c6fad4a13feb73a884d7c88ab9
Christian Georgii <christian.georgii@tweag.io>
2022-06-01 22:02:33 +0200

    Extract JWK setup

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,4 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 
 module Main where
 
@@ -7,31 +6,27 @@ import Api.AppServices (appServices)
 import Api.Application (app)
 import Api.Config (Port (..), apiConfig, apiPort)
 import qualified Api.Config as Config
-import CLIOptions (CLIOptions (configPath, jwkPath))
+import CLIOptions (CLIOptions (configPath))
 import qualified CLIOptions
-import Control.Exception (catch)
-import Crypto.JOSE.JWK (JWK)
-import Data.ByteString.Char8 (writeFile)
 import Data.Function ((&))
 import Network.Wai (Middleware)
 import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Middleware.Cors (cors, corsRequestHeaders, simpleCorsResourcePolicy)
 import Network.Wai.Middleware.RequestLogger (logStdoutDev)
-import Servant.Auth.Server (fromSecret, generateSecret, readKey)
-import Prelude hiding (writeFile)
 import qualified Tagger.Database as DB
+import qualified Tagger.JWK as JWK
 
 main :: IO ()
 main = do
   inputOptions <- CLIOptions.parse
   config <- Config.load (configPath inputOptions)
   dbHandle <- DB.new (DB.parseConfig config)
-  jsonWebKey <- setupJWK inputOptions
+  jsonWebKey <- JWK.setup inputOptions
   let port = apiPort . apiConfig $ config
 
   runApp jsonWebKey port dbHandle
 
-runApp :: JWK -> Port -> DB.Handle -> IO ()
+runApp :: JWK.JWK -> Port -> DB.Handle -> IO ()
 runApp jwk (Port port) dbHandle =
   let services = appServices dbHandle jwk
       application = app services & corsMiddleware & logStdoutDev
@@ -41,17 +36,3 @@ corsMiddleware :: Network.Wai.Middleware
 corsMiddleware =
   let headers = ["Authorization", "Content-Type"]
    in cors (const . Just $ simpleCorsResourcePolicy {corsRequestHeaders = headers})
-
-setupJWK :: CLIOptions -> IO JWK
-setupJWK =
-  jwtKey . jwkPath
-
-jwtKey :: FilePath -> IO JWK
-jwtKey path = do
-  -- try to retrieve the JWK from file
-  catch (readKey path) $ \(_ :: IOError) -> do
-    -- if the file does not exist or does not contain a valid key, we generate one
-    key <- generateSecret
-    -- and we store it
-    writeFile path key
-    pure $ fromSecret key
