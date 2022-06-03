686fffa154827e69b536b4e79fc649989a4db051
Christian Georgii <christian.georgii@tweag.io>
2022-06-03 18:35:03 +0200

    Implement Handle Pattern for Database module

--- src/Infrastructure/Database.hs
+++ src/Infrastructure/Database.hs
@@ -1,17 +1,51 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-module Infrastructure.Database (acquire) where
+module Infrastructure.Database
+  ( Config (..),
+    Handle,
+    withHandle,
+    runQuery,
+  )
+where
 
 import qualified Api.Config as AppConfig
-import qualified Data.ByteString.Char8 as BS
+import Data.ByteString.Char8 (ByteString, unpack)
 import Data.Maybe (fromMaybe)
-import qualified Hasql.Connection as Hasql
-import Hasql.Connection (Connection)
+import Hasql.Connection (Connection, acquire, release)
+import Hasql.Session (QueryError, Session, run)
+import Control.Exception (bracket)
 
-acquire :: AppConfig.Config -> IO Connection
-acquire config = do
-  eConn <- Hasql.acquire . AppConfig.connectionString . AppConfig.database $ config
+newtype Config = Config
+  { connectionString :: ByteString
+  }
+
+newtype Handle = Handle
+  { dbConnection :: Connection
+  }
+
+new :: Config -> IO Handle
+new config = do
+  eConn <- acquire . connectionString $ config
   either
-    (fail . BS.unpack . fromMaybe "unable to connect to the database")
-    pure
+    (fail . unpack . fromMaybe "unable to connect to the database")
+    (pure . Handle)
     eConn
+
+parseConfig :: AppConfig.Config -> Config
+parseConfig =
+  Config . (AppConfig.connectionString . AppConfig.database)
+
+close :: Handle -> IO ()
+close = release . dbConnection
+
+withHandle :: AppConfig.Config -> (Handle -> IO a) -> IO a
+withHandle config f = do
+  bracket
+    (new . parseConfig $ config)
+    close
+    f
+
+
+runQuery :: Handle -> Session a -> IO (Either QueryError a)
+runQuery handle query =
+  run query (dbConnection handle)
