9d0a808ff5c0bfc097d3e321bf8cacf0dc75ae7d
Christian Georgii <christian.georgii@tweag.io>
2022-06-13 17:05:44 +0200

    Refactor Logger module

--- app/Main.hs
+++ app/Main.hs
@@ -7,6 +7,8 @@ import qualified Api.Config as Config
 import CLIOptions (CLIOptions (configPath))
 import qualified CLIOptions
 import qualified Infrastructure.Database as DB
+import qualified Infrastructure.Logging.Logger as Logger
+import qualified Infrastructure.SystemTime as SystemTime
 import qualified Middleware
 import qualified Network.Wai.Handler.Warp as Warp
 import qualified Tagger.JSONWebKey as JWK
@@ -16,7 +18,8 @@ main = do
   options <- CLIOptions.parse
   appConfig <- Config.load $ configPath options
   key <- JWK.setup options
-  DB.withHandle appConfig (\dbHandle -> do
+  Deps.withDeps config (\Deps { dbHandle, loggerHandle }-> do
+    let services = appServices dbHandle loggerHandle key
     let (Port port) = apiPort . Config.api $ appConfig
         services = AppServices.start dbHandle key
         application = Middleware.apply (app services)
